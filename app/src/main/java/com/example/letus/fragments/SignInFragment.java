65b261327e6d9d3baf2f13179d0ea906f0eb897f
YaelDonat <donatyael@gmail.com>
2022-04-12 11:58:05 +0200

    Design changes, and other changes

--- app/src/main/java/com/example/letus/fragments/SignInFragment.java
+++ app/src/main/java/com/example/letus/fragments/SignInFragment.java
@@ -2,6 +2,8 @@ package com.example.letus.fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.util.Patterns;
 import android.view.LayoutInflater;
@@ -30,6 +32,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.sql.DatabaseMetaData;
+import java.util.Calendar;
 import java.util.regex.Pattern;
 
 public class SignInFragment extends AppCompatActivity implements View.OnClickListener {
@@ -54,6 +57,65 @@ public class SignInFragment extends AppCompatActivity implements View.OnClickLis
         editTextConfirmPassword = (EditText) findViewById(R.id.confirmPassword);
         editTextEmail = (EditText) findViewById(R.id.email);
         editTextBirthDate = (EditText) findViewById(R.id.birthdate);
+        editTextBirthDate.addTextChangedListener(new TextWatcher() {
+            private String current = "";
+            private String ddmmyyyy = "JJMMAAAA";
+            private Calendar cal = Calendar.getInstance();
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().equals(current)) {
+                    String clean = s.toString().replaceAll("[^\\d.]", "");
+                    String cleanC = current.replaceAll("[^\\d.]", "");
+
+                    int cl = clean.length();
+                    int sel = cl;
+                    for (int i = 2; i <= cl && i < 6; i += 2) {
+                        sel++;
+                    }
+                    //Fix for pressing delete next to a forward slash
+                    if (clean.equals(cleanC)) sel--;
+
+                    if (clean.length() < 8){
+                        clean = clean + ddmmyyyy.substring(clean.length());
+                    }else{
+                        //This part makes sure that when we finish entering numbers
+                        //the date is correct, fixing it otherwise
+                        int day  = Integer.parseInt(clean.substring(0,2));
+                        int mon  = Integer.parseInt(clean.substring(2,4));
+                        int year = Integer.parseInt(clean.substring(4,8));
+
+                        if(mon > 12) mon = 12;
+                        cal.set(Calendar.MONTH, mon-1);
+
+                        year = (year<1900)?1900:(year>2100)?2100:year;
+                        cal.set(Calendar.YEAR, year);
+                        // ^ first set year for the line below to work correctly
+                        //with leap years - otherwise, date e.g. 29/02/2012
+                        //would be automatically corrected to 28/02/2012
+
+                        day = (day > cal.getActualMaximum(Calendar.DATE))? cal.getActualMaximum(Calendar.DATE):day;
+                        clean = String.format("%02d%02d%02d",day, mon, year);
+                    }
+
+                    clean = String.format("%s/%s/%s", clean.substring(0, 2),
+                            clean.substring(2, 4),
+                            clean.substring(4, 8));
+
+                    sel = sel < 0 ? 0 : sel;
+                    current = clean;
+                    editTextBirthDate.setText(current);
+                    editTextBirthDate.setSelection(sel < current.length() ? sel : current.length());
+                }
+            }
+
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
         checkBoxAgree = (CheckBox) findViewById(R.id.checkbox_agree);
         buttonCreateAcc = (Button) findViewById(R.id.buttonSaveContact);
         buttonCreateAcc.setOnClickListener(this);
